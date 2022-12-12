76c0412590eb5e55e2034b7b1da74cd2d8135430
Martin Studer <martin.studer@medi.ch>
2022-12-12 16:34:32 +0100

    added user as configuration

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 ## Provides
 A docker container with a sftp user "share".
 ## Required
-A file with a password entry placed at /run/secrets/sftp_share_password. You can provide this for example with a docker compose configuration file.
+Secrets:
+- sftp_share_login
+- sftp_share_password
--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 set -e
 
-export USER_PWD=$(cat /run/secrets/sftp_share_password)
+export USER_LOGIN=$(cat /run/secrets/sftp_share_login)
+export USER_PASSWORD=$(cat /run/secrets/sftp_share_password)
 
 echo "generate ssh key..."
 cd /etc/ssh
 ssh-keygen -A
 
-echo "create group share | gid 1000..."
-groupadd --gid 1000 share
+echo "create group " $USER_LOGIN " | gid 1000..."
+groupadd --gid 1000 $USER_LOGIN
 
-echo "create user share..."
-useradd -d /home/share -g share share
-echo share:$USER_PWD | chpasswd
+echo "create user " $USER_LOGIN "..."
+useradd -d /home/share -g $USER_LOGIN $USER_LOGIN
+echo $USER_LOGIN:$USER_PASSWORD | chpasswd
 
-echo "create home directory for user share..."
+echo "create home directory for user " $USER_LOGIN "..."
 mkdir -p /home/share
-chown -R share:share /home/share
+chown -R $USER_LOGIN:$USER_LOGIN /home/share
 chmod 700 -R /home/share
 
 echo "Executing sshd..."
